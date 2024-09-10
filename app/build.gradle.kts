c1af1e9cd49029649d87a0b14d713afb3951f1b3
Sacs <sergio.acs@hotmail.com>
2024-09-10 18:21:51 -0500

    gradle estable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,7 @@ dependencies {
 
 
     implementation("androidx.navigation:navigation-compose:2.5.3")
+    implementation(libs.androidx.runtime.livedata)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
